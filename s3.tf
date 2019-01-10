da71650c838f5dc63479f1107e58c2944d11212b
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-01-10 07:49:35 -0600

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,20 @@
+# S3 bucket config
+
+resource "aws_s3_bucket" "examples3" {
+  # NOTE: S3 bucket names must be unique across _all_ AWS accounts, so
+  # this name must be changed before applying this example to avoid naming
+  # conflicts.
+  bucket = "vinnie-examples3-11223344"
+  acl    = "private"
+  
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm     = "AES256"
+      }
+    }
+  }
+  tags {
+    website = "true"
+  } 
+}
