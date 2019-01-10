4d7dc96719ace454f832231b0533fb72cbbff9f3
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-01-10 09:07:30 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,16 @@
 provider "aws" {}
 
 #EC2 INSTANCE(s)//-------------------------------------------------------------
+#Trusted AMI: ami-009d6802948d06e52
+#Trusted Subnet: subnet-05f417996108c1f5f
 
 resource "aws_instance" "ec2" {
   count = "1"
-  ami           = "ami-009d6802948d06e52"
+  ami           = "ami-009d6802948d06e00"
   instance_type = "t2.micro"
 
   # VPC subnet
-  subnet_id = "subnet-05f417996108c1f5f"
+  subnet_id = "subnet-05f417996108c1f00"
 
   # security group
   # vpc_security_group_ids = ["sg-0d1b21350b7259316"]
